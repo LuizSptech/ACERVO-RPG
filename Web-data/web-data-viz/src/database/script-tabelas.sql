ef21b581605c359480cab71b8ccbd9461c442905
Loouiz <luiz.correia@sptech.school>
2026-05-24 20:51:18 -0300

    versão-final-SCRIPT

--- Web-data/web-data-viz/src/database/script-tabelas.sql
+++ Web-data/web-data-viz/src/database/script-tabelas.sql
@@ -1,22 +1,33 @@
-CREATE DATABASE RPGACERVO;
-
+create database RPGACERVO;
 use RPGACERVO;
 
+-- Tabela Tipo
+CREATE TABLE Tipo (
+    idQuestionario INT          NOT NULL AUTO_INCREMENT,
+    nome           VARCHAR(45)  NOT NULL,
+    pontuacao      INT          NOT NULL,
+    descricao      VARCHAR(120),
+    PRIMARY KEY (idQuestionario)
+);
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50) not null,
-	email VARCHAR(50) not null unique,
-	senha VARCHAR(50) not null
-    );
+-- Tabela Usuarios
+CREATE TABLE Usuarios (
+    idUsuarios INT          NOT NULL AUTO_INCREMENT,
+    nome       VARCHAR(50)  NOT NULL,
+    email      VARCHAR(50)  NOT NULL,
+    senha      VARCHAR(50)  NOT NULL,
+    PRIMARY KEY (idUsuarios)
+);
 
-create table esteriotipos (
-	id int primary key auto_increment,
-    nome varchar (50),
-    descricao text
-    );
-
-select * from usuario;
-
-
-desc usuario;
+-- Tabela Quiz
+CREATE TABLE Quiz (
+    idQuizz    INT      NOT NULL AUTO_INCREMENT,
+    fkUsuario  INT      NOT NULL,
+    fkTipo     INT      NOT NULL,
+    data       DATETIME NOT NULL,
+    PRIMARY KEY (idQuizz),
+    CONSTRAINT fk_quiz_usuario
+        FOREIGN KEY (fkUsuario) REFERENCES Usuarios (idUsuarios),
+    CONSTRAINT fk_quiz_tipo
+        FOREIGN KEY (fkTipo)    REFERENCES Tipo (idQuestionario)
+);
