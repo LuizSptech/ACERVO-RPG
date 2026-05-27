fa05cb8ca8206e4009bd1597dbd4457cf94c4a15
LuizSptech <luiz.correia@sptech.school>
2026-05-26 21:06:29 -0300

    salvando pontuação

--- Web-data/web-data-viz/src/models/quizzModel.js
+++ Web-data/web-data-viz/src/models/quizzModel.js
@@ -1,48 +1,46 @@
 var database = require("../database/config")
 
-// function autenticar(email, senha) {
-//     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
-//     var instrucaoSql = `
-//         SELECT id, nome, email FROM usuario WHERE email = '${email}' AND senha = '${senha}';
-//     `;
-//     console.log("Executando a instrução SQL: \n" + instrucaoSql);
-//     return database.executar(instrucaoSql);
-// }
-
-// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
 function guardar(usuario, resultado, valores) {
+
     console.log("Salvando quiz");
-    
-    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-    //  e na ordem de inserção dos dados.
+
     var instrucaoSql = `
         INSERT INTO Quiz (fkUsuario, fkTipoResultado)
-        VALUES (${usuario}, ${resultado});`;
+        VALUES (${usuario}, ${resultado});
+    `;
 
     console.log(instrucaoSql);
 
-    return database.executar(instrucaoSql).then(function(respostaQuiz){
+    return database.executar(instrucaoSql)
+
+    .then(function(respostaQuiz){
+
         let idQuiz = respostaQuiz.insertId;
 
-        let  prometa = [];
+        let promessas = [];
 
-        for(let i = 0; i < valores.lenght; i++){
-            let pontos = valores[i]
+        for(let i = 0; i < valores.length; i++){
+
+            let pontos = valores[i];
 
             let fkTipo = i + 1;
 
             let instrucaoResultado = `
-            INSERT INTO ResultadoQuiz (
-            fkQuiz, fkTipo, pontuacao) VALUES
-            (${idQuiz}, ${fkTipo}, ${pontos});`;
+                INSERT INTO ResultadoQuiz
+                (fkQuiz, fkTipo, pontuacao)
+                VALUES
+                (${idQuiz}, ${fkTipo}, ${pontos});
+            `;
 
             console.log(instrucaoResultado);
 
-            prometa.push(
+            promessas.push(
                 database.executar(instrucaoResultado)
             );
         }
-        return Promise.all(prometa);
+
+        return Promise.all(promessas);
+
     });
 
 }
